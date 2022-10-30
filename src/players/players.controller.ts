851a9a62faff33b8fad340b3816668a499c611f9
mavismmg <contatomateusjorgejf@gmail.com>
2022-10-30 00:02:35 -0300

    feat(method): update player name if email is the same

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { Player } from './interfaces/player.interface';
 import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+    
     constructor(private readonly playersService: PlayersService) {}
+
+    @Get()
+    async getAllPlayers(): Promise<Player[]> {
+        return this.playersService.getAllPlayers()
+    }
 
     @Post()
     async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -5,11 +5,27 @@ import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class PlayersService { 
+    
     private readonly logger = new Logger('PlayersService.name');
     private players: Player[] = [];
 
+    async getAllPlayers(): Promise<Player[]> {
+        return await this.players;
+    }
+
     async createUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
+        const { email } = createPlayerDto;
+        const playerFound = await this.players.find(player => player.email === email);
+        if (playerFound) {
+            return await this.update(playerFound, createPlayerDto);
+        }
+
         await this.create(createPlayerDto);
+    }
+
+    private async update(playerFound: Player, createPlayerDto: CreatePlayerDto): Promise<void> {
+        const { name } = createPlayerDto;
+        playerFound.name = name;
     }
 
     private async create(createPlayerDto: CreatePlayerDto): Promise<void> {
