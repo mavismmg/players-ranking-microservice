92fcfc61e59deef76f060b6fccae1f3092d8092a
mavismmg <contatomateusjorgejf@gmail.com>
2022-11-02 13:37:30 -0300

    feat(test): created new tests

--- src/players/players.controller.spec.ts
+++ src/players/players.controller.spec.ts
@@ -1,17 +1,33 @@
+import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { Model } from 'mongoose';
-import { Player } from './interfaces/player.interface';
-import { PlayerSchema } from './interfaces/player.schema';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { Response } from 'express';
 
 const mockPlayersModel = () => ({
-  // TODO: mock players model.
+  viewPlayers: jest.fn(),
+  viewPlayerByName: jest.fn(),
+  createOrUpdatePlayerByEmail: jest.fn(),
 });
 
+const mockPlayer = {
+  phoneNumber: 'somePhoneNumber',
+  email: 'test@mail.com',
+  name: 'test',
+};
+
+const statusResponseMock = {
+  send: jest.fn((x) => x),
+};
+
+const responseMock = {
+  status: jest.fn((x) => statusResponseMock),
+  send: jest.fn((x) => x),
+} as unknown as Response;
+
 describe('PlayersController', () => {
-  let playersService: PlayersService;
   let playersController: PlayersController;
+  let playersModel: any;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       controllers: [PlayersController],
@@ -20,18 +36,39 @@ describe('PlayersController', () => {
         { provide: PlayersService, useFactory: mockPlayersModel },
       ],
     }).compile();
-    playersService = module.get<PlayersService>(PlayersService);
     playersController = module.get<PlayersController>(PlayersController);
+    playersModel = module.get<PlayersService>(PlayersService);
   });
 
   it('should be defined', () => {
-    expect(playersService).toBeDefined();
+    expect(playersModel).toBeDefined();
     expect(playersController).toBeDefined();
   });
 
-  describe('viewPlayerByEmail', () => {
-    it('calls PlayersController.viewPlayerByEmail and returns the result', async () => {
-      // TODO: tests.
+  describe('playersOrPlayerWithAttribute', () => {
+    it('calls PlayersController.playersOrPlayerWithAttribute and returns a player or a list of players', async () => {
+      playersModel.viewPlayers.mockResolvedValue('attribute');
+      const resultWithQueryParam =
+        await playersController.playersOrPlayerWithAttribute();
+      expect(resultWithQueryParam).toEqual('attribute');
+      playersModel.viewPlayers.mockResolvedValue([]);
+      const resultWithoutQueryParam =
+        await playersController.playersOrPlayerWithAttribute();
+      expect(resultWithoutQueryParam).toEqual([]);
+    });
+  });
+
+  describe('playerByName', () => {
+    it('calls PlayersController.playerByName and returns a player by name', async () => {
+      playersModel.viewPlayerByName.mockResolvedValue('name');
+      const result = await playersController.playerByName(null);
+      expect(result).toEqual('name');
+    });
+  });
+
+  describe('createOrUpdatePlayerByEmail', () => {
+    it('calls PlayersController.createOrUpdatePlayerByEmail and returns a created or updated player', async () => {
+      //
     });
   });
 });
