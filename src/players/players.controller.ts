a3fc080af00ee83b0df9ef24e30e983d4fa8125e
mavismmg <contatomateusjorgejf@gmail.com>
2022-11-02 23:09:47 -0300

    refactor(returns): implemented responses and changed returns types

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -27,29 +27,40 @@ export class PlayersController {
     return await this.getPlayerByName(name);
   }
 
+  public async playerByRanking(ranking: string): Promise<Player[]> {
+    return await this.getPlayerByRanking(ranking);
+  }
+
   public async createOrUpdatePlayerByEmail(
     createPlayerDto: CreatePlayerDto,
     response: Response,
-  ): Promise<void> {
-    await this.createUpdatePlayerByEmail(createPlayerDto, response);
+  ): Promise<Response> {
+    return await this.createUpdatePlayerByEmail(createPlayerDto, response);
   }
 
   public async createOrUpdatePlayerByPhoneNumber(
     createPlayerDto: CreatePlayerDto,
-  ): Promise<void> {
-    await this.createUpdatePlayerByPhoneNumber(createPlayerDto);
+    respose: Response,
+  ): Promise<Response> {
+    return await this.createUpdatePlayerByPhoneNumber(createPlayerDto, respose);
   }
 
-  public async deleteByEmail(email: string): Promise<void> {
-    await this.deletePlayerByEmail(email);
+  public async deleteByEmail(
+    email: string,
+    response: Response,
+  ): Promise<Response> {
+    return await this.deletePlayerByEmail(email, response);
   }
 
-  public async deleteByPhoneNumber(phoneNumber: string): Promise<void> {
-    await this.deletePlayerByPhoneNumber(phoneNumber);
+  public async deleteByPhoneNumber(
+    phoneNumber: string,
+    response: Response,
+  ): Promise<any> {
+    return await this.deletePlayerByPhoneNumber(phoneNumber, response);
   }
 
-  public async deleteByName(name: string): Promise<void> {
-    await this.deletePlayersByName(name);
+  public async deleteByName(name: string, response: Response): Promise<any> {
+    return await this.deletePlayersByName(name, response);
   }
 
   @Get()
@@ -66,6 +77,11 @@ export class PlayersController {
     }
   }
 
+  @Get('id')
+  private async getPlayerById(@Query('id') id: string): Promise<Player> {
+    return await this.playersService.viewPlayerById(id);
+  }
+
   @Get('name')
   private async getPlayerByName(
     @Query('name') name: string,
@@ -73,47 +89,84 @@ export class PlayersController {
     return await this.playersService.viewPlayerByName(name);
   }
 
+  @Get('ranking')
+  private async getPlayerByRanking(
+    @Query('ranking') ranking: string,
+  ): Promise<Player[]> {
+    return await this.playersService.viewPlayerByRanking(ranking);
+  }
+
+  @Get('rankingPosition')
+  private async getPlayerByRankingPosition(
+    @Query('rankingPosition') rankingPosition: number,
+  ): Promise<Player> {
+    return await this.playersService.viewPlayerByRankingPosition(
+      rankingPosition,
+    );
+  }
+
   @Post('email')
   private async createUpdatePlayerByEmail(
     @Body(new ValidationPipe()) createPlayerDto: CreatePlayerDto,
-    @Res() res: Response,
+    @Res() response: Response,
   ): Promise<Response> {
-    const request = await this.playersService.createOrUpdatePlayerByEmail(
-      createPlayerDto,
-    );
-    if (request)
-      return res
-        .status(HttpStatus.CREATED)
-        .send({ status: 201, message: 'success' });
+    const isPlayerCreated =
+      await this.playersService.createOrUpdatePlayerByEmail(createPlayerDto);
+    if (isPlayerCreated)
+      return response.status(HttpStatus.CREATED).send({ status: 201 });
   }
 
   @Post('phone')
   private async createUpdatePlayerByPhoneNumber(
     @Body(new ValidationPipe()) createPlayerDto: CreatePlayerDto,
+    @Res() response: Response,
+  ): Promise<Response> {
+    const isPlayerCreated =
+      await this.playersService.createOrUpdatePlayerByPhoneNumber(
+        createPlayerDto,
+      );
+    if (isPlayerCreated)
+      return response.status(HttpStatus.CREATED).send({ status: 201 });
+  }
+
+  @Post('photo')
+  private async updatePlayerPhoto(
+    @Body(new ValidationPipe()) createPlayerDto: CreatePlayerDto,
   ): Promise<void> {
-    await this.playersService.createOrUpdatePlayerByPhoneNumber(
-      createPlayerDto,
-    );
+    await this.playersService.updatePlayerPhoto(createPlayerDto);
   }
 
   @Delete('email')
   private async deletePlayerByEmail(
     @Query('email', PlayersValidationParametersPipe) email: string,
-  ): Promise<void> {
-    this.playersService.deletePlayerByEmail(email);
+    @Res() response: Response,
+  ): Promise<Response> {
+    const isPlayerDeleted = await this.playersService.deletePlayerByEmail(
+      email,
+    );
+    if (!isPlayerDeleted)
+      return response.status(HttpStatus.ACCEPTED).send({ status: 200 });
   }
 
   @Delete('phoneNumber')
   private async deletePlayerByPhoneNumber(
     @Query('phone') phoneNumber: string,
-  ): Promise<void> {
-    this.playersService.deletePlayerByPhoneNumber(phoneNumber);
+    @Res() response: Response,
+  ): Promise<Response> {
+    const isPlayerDeleted = await this.playersService.deletePlayerByPhoneNumber(
+      phoneNumber,
+    );
+    if (!isPlayerDeleted)
+      return response.status(HttpStatus.ACCEPTED).send({ status: 200 });
   }
 
   @Delete('name')
   private async deletePlayersByName(
     @Query('name') name: string,
-  ): Promise<void> {
-    this.playersService.deletePlayerByName(name);
+    @Res() response: Response,
+  ): Promise<Response> {
+    const isPlayerDeleted = await this.playersService.deletePlayerByName(name);
+    if (!isPlayerDeleted)
+      return response.status(HttpStatus.ACCEPTED).send({ status: 200 });
   }
 }
